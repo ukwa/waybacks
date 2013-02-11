a950956736685120015fc1e966f4e086d0b9843c
Andrew Jackson <Andrew.Jackson@bl.uk>
2013-02-11 09:49:23 +0000

    Made the deployment config easier to override in 'child' overlay WARs.

--- wayback-ukwa/src/main/webapp/js/javascript-overrides.js
+++ wayback-ukwa/src/main/webapp/js/javascript-overrides.js
@@ -1,4 +1,10 @@
 /**
+ * References:
+ * 
+ *  - Overriding without polluting the global namespace: http://stackoverflow.com/a/9172526/6689
+ */
+
+/**
  * Override window.XMLHttpRequest invocations to refer to the archival host.
  */
 window.XMLHttpRequestBase = window.XMLHttpRequest; 
@@ -14,7 +20,6 @@ window.XMLHttpRequest = function() {
 }
 
 
-
 /**
  * Override window.open invocations to refer to the archival host.
  */
@@ -23,6 +28,7 @@ window.openBase = window.open;
 window.open = function() {
   var base = new window.openBase(); 
   base.openBase = base.open; 
+  
   //
   // https://developer.mozilla.org/en-US/docs/DOM/window.open
   // var windowObjectReference = window.open(strUrl, strWindowName[, strWindowFeatures]);
@@ -30,9 +36,10 @@ window.open = function() {
   // http://msdn.microsoft.com/en-gb/library/ie/ms536651(v=vs.85).aspx
   // var retval = window.open(url, name, features, replace);
   
-  base.open = function(sMethod, sUrl, bAsync, sUser, sPassword){
-      console.log("XHR "+sMethod+": "+sUrl); //manipulate sUrl here return 
-	  base.openBase(sMethod, sUrl, bAsync, sUser, sPassword);
+  base.open = function(sUrl, strWindowName, features, replace ){
+      console.log("OPEN "+strWindowName+": "+sUrl); //manipulate sUrl here return 
+      alert("OPEN "+strWindowName+": "+sUrl);
+	  base.openBase(sUrl, strWindowName, features, replace);
   }
   return base;
 }
