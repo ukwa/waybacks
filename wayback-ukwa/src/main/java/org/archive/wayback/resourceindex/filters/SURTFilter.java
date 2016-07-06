4c408cdbadd96dc2a60b7788e17458a52ce8e320
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-07-06 15:46:52 +0100

    Default to whitelist logic.

--- wayback-ukwa/src/main/java/org/archive/wayback/resourceindex/filters/SURTFilter.java
+++ wayback-ukwa/src/main/java/org/archive/wayback/resourceindex/filters/SURTFilter.java
@@ -11,43 +11,44 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SURTFilter implements ObjectFilter<CaptureSearchResult> {
-    public final static char LEGAL_DEPOSIT_FLAG = 'L';
-    public final static char OPEN_LICENCE_FLAG = 'O';
-    public final static char EMPTY_FLAG = '-';
+	public final static char LEGAL_DEPOSIT_FLAG = 'L';
+	public final static char OPEN_LICENCE_FLAG = 'O';
+	public final static char EXCLUDE_FLAG = 'X';
+	public final static char EMPTY_FLAG = '-';
 
-    protected static Logger logger = LoggerFactory.getLogger(SURTFilter.class);
-    private int filter;
+	protected static Logger logger = LoggerFactory.getLogger(SURTFilter.class);
+	private int filter;
 
-    public SURTFilter(WaybackRequest request, SurtPrefixSet permittedSurts) {
-        try {
-            UsableURI uuri = UsableURIFactory.getInstance(request
-                    .getRequestUrl());
-            String surt = SurtPrefixSet.getCandidateSurt(uuri);
-            if (permittedSurts.containsPrefixOf(surt)) {
-                filter = FILTER_INCLUDE;
-                logger.debug("Allowing " + uuri.toString() + " (" + surt + ")");
-            } else {
-                filter = FILTER_EXCLUDE;
-                logger.debug("Denying " + uuri.toString() + " (" + surt + ")");
-            }
-        } catch (URIException e) {
-            logger.warn(e.getMessage());
-            filter = FILTER_EXCLUDE;
-        }
-    }
+	public SURTFilter(WaybackRequest request, SurtPrefixSet permittedSurts) {
+		try {
+			UsableURI uuri = UsableURIFactory.getInstance(request
+					.getRequestUrl());
+			String surt = SurtPrefixSet.getCandidateSurt(uuri);
+			if (permittedSurts.containsPrefixOf(surt)) {
+				filter = FILTER_INCLUDE;
+				logger.info("Allowing " + uuri.toString() + " (" + surt + ")");
+			} else {
+				filter = FILTER_EXCLUDE;
+				logger.info("Denying " + uuri.toString() + " (" + surt + ")");
+			}
+		} catch (URIException e) {
+			logger.warn(e.getMessage());
+			filter = FILTER_EXCLUDE;
+		}
+	}
 
-    @Override
-    public int filterObject(CaptureSearchResult r) {
-        char flag = r.getRobotFlags().charAt(0);
-        switch (flag) {
-        case OPEN_LICENCE_FLAG:
-            return FILTER_INCLUDE;
-        case EMPTY_FLAG:
-            return FILTER_EXCLUDE;
-        case LEGAL_DEPOSIT_FLAG:
-            return filter;
-        default:
-            return FILTER_EXCLUDE;
-        }
-    }
+	@Override
+	public int filterObject(CaptureSearchResult r) {
+		char flag = r.getRobotFlags().charAt(0);
+		switch (flag) {
+		case OPEN_LICENCE_FLAG:
+			return FILTER_INCLUDE;
+		case EXCLUDE_FLAG:
+			return FILTER_EXCLUDE;
+		case LEGAL_DEPOSIT_FLAG:
+			return filter;
+		default:
+			return filter;
+		}
+	}
 }
