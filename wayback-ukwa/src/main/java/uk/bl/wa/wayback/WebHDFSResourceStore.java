29bcc766dc76c55879bd40807d8a33d6a1ad3948
Andrew Jackson <Andrew.Jackson@bl.uk>
2017-01-27 11:52:38 +0000

    Made the WebHFDS support more usable.

--- wayback-ukwa/src/main/java/uk/bl/wa/wayback/WebHDFSResourceStore.java
+++ wayback-ukwa/src/main/java/uk/bl/wa/wayback/WebHDFSResourceStore.java
@@ -12,14 +12,18 @@ import org.archive.wayback.resourcestore.SimpleResourceStore;
 
 /**
  * 
- * WebHDFS does not support Range requests (see https://issues.apache.org/jira/browse/HDFS-2316?focusedCommentId=13138726&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-13138726)
+ * WebHDFS does not support Range requests (see
+ * https://issues.apache.org/jira/browse/HDFS-2316?focusedCommentId=13138726&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-13138726)
  * 
- *     "> 2. ranges
- *      Since it is optional in the http spec, let's use offset and length query parameters."
+ * "> 2. ranges Since it is optional in the http spec, let's use offset and
+ * length query parameters."
  * 
  * So this class maps a vanilla request into a WebHDFS-style offset request.
  * 
- * Use the 'prefix' parameter from SimpleResourceStore to set the host and port of the WebHDFS service.
+ * Use the 'prefix' parameter from SimpleResourceStore to set the host:port/path
+ * of the WebHDFS service,
+ * 
+ * e.g. http://hadoop:50070/webhdfs/v1
  * 
  * @author AnJackson
  *
@@ -29,9 +33,31 @@ public class WebHDFSResourceStore extends SimpleResourceStore {
 	private final static Logger LOGGER = Logger.getLogger(
 			WebHDFSResourceStore.class.getName());
 
-	/* (non-Javadoc)
-	 * @see org.archive.wayback.resourcestore.SimpleResourceStore#retrieveResource(org.archive.wayback.core.CaptureSearchResult)
-	 */
+    private String hdfsUser = "hdfs";
+
+    /**
+     * 
+     * @return
+     */
+    public String getHdfsUser() {
+        return hdfsUser;
+    }
+
+    /**
+     * 
+     * @param hdfsUser
+     */
+    public void setHdfsUser(String hdfsUser) {
+        this.hdfsUser = hdfsUser;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.archive.wayback.resourcestore.SimpleResourceStore#retrieveResource(
+     * org.archive.wayback.core.CaptureSearchResult)
+     */
 	@Override
 	public Resource retrieveResource(CaptureSearchResult result)
 			throws ResourceNotAvailableException {
@@ -43,7 +69,15 @@ public class WebHDFSResourceStore extends SimpleResourceStore {
 		// Remove any existing fragment identifier:s
 		fileName = fileName.replaceFirst("#.*$", "");
 		// Add WebHDFS parameters:
-		fileName = "/webhdfs/v1" + fileName + "?user.name=hdfs&op=OPEN&offset="+result.getOffset()+"#.warc.gz";
+        if (result.getCompressedLength() > 0) {
+            fileName = fileName + "?user.name=" + this.hdfsUser
+                    + "&op=OPEN&offset=" + result.getOffset() + "&length="
+                    + result.getCompressedLength() + "#.warc.gz";
+        } else {
+            fileName = fileName + "?user.name=" + this.hdfsUser
+                    + "&op=OPEN&offset="
+                + result.getOffset() + "#.warc.gz";
+        }
 		// Update the search result object:
 		result.setFile(fileName);
 		result.setOffset(0);
