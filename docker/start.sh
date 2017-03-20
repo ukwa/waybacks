2a750171eaeabb3542ffea546ad23cc0aaaaa1b0
Andrew Jackson <Andrew.Jackson@bl.uk>
2017-03-20 22:52:38 +0000

    Fix version config flag and add compose file.

--- docker/start.sh
+++ docker/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Deploy selected WAR
-cp /waybacks/wayback-qa/target/*.war /opt/tomcat/webapps/ROOT.war
+cp /waybacks/wayback-${UKWA_OWB_VERSION}/target/*.war /opt/tomcat/webapps/ROOT.war
 
 # Boot Tomcat:
 /opt/tomcat/bin/startup.sh && tail -F /opt/tomcat/logs/catalina.out
